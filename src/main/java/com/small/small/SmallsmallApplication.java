5e1407e1ce466a5469189d68559e0b2d05e421c2
wessonjava <work_wmd@163.com>
2020-09-16 21:53:14 +0800

    init

---
+++ src/main/java/com/small/small/SmallsmallApplication.java
@@ -0,0 +1,13 @@
+package com.small.small;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallsmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallsmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/small/small/SmallsmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.small.small;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallsmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
